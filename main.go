43a77fa40e0d98e3412ad452dbd4443170073d73
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2023-01-30 19:45:23 -0800

    Add working subcommands
    Also did everything.
    
    Co-Authored-By: diamondburned <diamond@arikawa-hi.me>

--- main.go
+++ main.go
@@ -3,17 +3,18 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"log"
 	"os"
 	"path/filepath"
 
+	arikawa "github.com/diamondburned/arikawa/v3/discord"
 	"github.com/joho/godotenv"
 	"github.com/pkg/errors"
 	"github.com/urfave/cli/v2"
 
-	"github.com/ethanthatonekid/pins/preprocess"
-	"github.com/ethanthatonekid/pins/preprocess/discord"
+	"github.com/ethanthatonekid/pins/pins"
+	"github.com/ethanthatonekid/pins/pins/discord"
 )
 
 func main() {
@@ -33,85 +34,164 @@ func NewApp() *App {
 	app := &App{}
 
 	app.App = &cli.App{
-		Name:     "pins",
-		HelpName: "get pins from Discord",
+		Name:      "pins",
+		Usage:     "get pins from Discord",
+		ArgsUsage: "guildID [expression]",
 		Flags: []cli.Flag{
-			&cli.StringFlag{
-				Name:    "guild",
-				Aliases: []string{"g"},
-				Usage:   "the guild ID to save pins from",
-			},
-			&cli.StringSliceFlag{
-				Name:    "skip-channel",
-				Aliases: []string{"sc"},
-				Usage:   "skipped if the channel contains the regex pattern",
-			},
-			&cli.StringSliceFlag{
-				Name:    "keep-channel",
-				Aliases: []string{"kc"},
-				Usage:   "skipped if the channel does not contain the regex pattern",
-			},
-			&cli.StringSliceFlag{
-				Name:    "skip-message",
-				Aliases: []string{"sm"},
-				Usage:   "skipped if the pin contains the regex pattern",
-			},
-			&cli.StringSliceFlag{
-				Name:    "keep-message",
-				Aliases: []string{"km"},
-				Usage:   "skipped if the pin does not contain the regex pattern",
-			},
-			&cli.PathFlag{
-				Name:    "output",
-				Aliases: []string{"o"},
-				Value:   "output",
-				Usage:   "the output directory",
-			},
 			&cli.BoolFlag{
 				Name:    "verbose",
 				Aliases: []string{"v"},
 				Usage:   "print information about each pin",
 			},
+			&cli.BoolFlag{
+				Name:    "dry-run",
+				Aliases: []string{"d"},
+				Usage:   "don't save pins",
+			},
 		},
-		Action: action,
+		Before: func(ctx *cli.Context) error {
+			if ctx.Bool("verbose") {
+				log.SetOutput(ctx.App.ErrWriter)
+				log.SetFlags(log.Ltime)
+			} else {
+				log.SetOutput(io.Discard)
+			}
+			return nil
+		},
+		After: func(ctx *cli.Context) error {
+			log.SetOutput(ctx.App.ErrWriter)
+			return nil
+		},
+		Commands: []*cli.Command{
+			{
+				Name:      "get",
+				Action:    getAction,
+				Usage:     "get pins from a guild",
+				ArgsUsage: "guildID [expression]",
+				Flags: []cli.Flag{
+					&cli.PathFlag{
+						Name:    "output",
+						Aliases: []string{"o"},
+						Value:   "output",
+						Usage:   "the output directory",
+					},
+				},
+			},
+			{
+				Name:      "update",
+				Action:    updateAction,
+				Usage:     "update the output file with new pins",
+				ArgsUsage: "file",
+			},
+			{
+				Name:      "print-expr",
+				Action:    printExprAction,
+				Usage:     "print the expression used to run this tool from a path",
+				ArgsUsage: "filepath",
+			},
+		},
 	}
 
 	return app
 }
 
-func action(ctx *cli.Context) error {
+func getAction(ctx *cli.Context) error {
 	provider := discord.New("Bot " + os.Getenv("DISCORD_TOKEN"))
 
-	outputRoot := ctx.Path("output")
-	guildID := ctx.String("guild")
-	verbose := ctx.Bool("verbose")
+	arg1 := ctx.Args().Get(0)
+	expr := pins.FilterExpr(ctx.Args().Get(1))
 
-	data, err := preprocess.RenderGuild(provider, preprocess.RenderGuildOptions{
-		GuildID:      guildID,
-		SkipChannels: ctx.StringSlice("skip-channel"),
-		KeepChannels: ctx.StringSlice("keep-channel"),
-		SkipMessages: ctx.StringSlice("skip-message"),
-		KeepMessages: ctx.StringSlice("keep-message"),
-	})
+	snowflake, err := arikawa.ParseSnowflake(arg1)
 	if err != nil {
-		return errors.Wrap(err, "failed to render guild")
+		return errors.Wrap(err, "failed to parse snowflake")
 	}
 
-	if verbose {
-		fmt.Println(data)
+	guildID := arikawa.GuildID(snowflake)
+
+	dryRun := ctx.Bool("dry-run")
+	outputRoot := ctx.Path("output")
+
+	data, err := provider.Pins(pins.PinsOptions{
+		GuildID: guildID,
+		Expr:    expr,
+	})
+	if err != nil {
+		return errors.Wrap(err, "failed to get pins")
+	}
+
+	log.Println(data)
+
+	if dryRun {
+		return nil
 	}
 
 	if err := os.MkdirAll(outputRoot, 0755); err != nil {
 		return errors.Wrap(err, "failed to create output directory")
 	}
 
-	outputFile := filepath.Join(outputRoot, guildID+".json")
-	output, err := json.MarshalIndent(data, "", "  ")
+	filename, err := encodeFilename(guildID, expr)
+	if err != nil {
+		return errors.Wrap(err, "failed to encode filename")
+	}
+
+	outputPath := filepath.Join(outputRoot, filename)
+
+	content, err := json.MarshalIndent(data, "", "  ")
 	if err != nil {
 		return errors.Wrap(err, "failed to marshal data")
 	}
 
-	if err := ioutil.WriteFile(outputFile, output, 0644); err != nil {
+	if err := os.WriteFile(outputPath, content, 0644); err != nil {
+		return errors.Wrap(err, "failed to write output file")
+	}
+
+	return nil
+}
+
+func printExprAction(ctx *cli.Context) error {
+	path := ctx.Args().Get(0)
+	if path == "" {
+		return errors.New("no file provided, check -h")
+	}
+
+	guildID, hash, err := decodeFilename(filepath.Base(path))
+	if err != nil {
+		return errors.Wrap(err, "failed to decode filename")
+	}
+
+	fmt.Println("Copy the following flags to run this tool:")
+	fmt.Printf("  --guild %q --expr %q\n", guildID, hash)
+
+	return nil
+}
+
+func updateAction(ctx *cli.Context) error {
+	provider := discord.New("Bot " + os.Getenv("DISCORD_TOKEN"))
+
+	path := ctx.Args().Get(0)
+	if path == "" {
+		return errors.New("no file provided, check -h")
+	}
+
+	guildID, expr, err := decodeFilename(filepath.Base(path))
+	if err != nil {
+		return errors.Wrap(err, "failed to decode filename")
+	}
+
+	data, err := provider.Pins(pins.PinsOptions{
+		GuildID: guildID,
+		Expr:    expr,
+	})
+	if err != nil {
+		return errors.Wrap(err, "failed to get pins")
+	}
+
+	content, err := json.MarshalIndent(data, "", "  ")
+	if err != nil {
+		return errors.Wrap(err, "failed to marshal data")
+	}
+
+	if err := os.WriteFile(path, content, 0644); err != nil {
 		return errors.Wrap(err, "failed to write output file")
 	}
 
